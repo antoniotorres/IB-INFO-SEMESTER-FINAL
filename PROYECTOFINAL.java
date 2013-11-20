4ab18211b15691db7babdddc763a32dad510d0fd
Antonio <thetonio96@gmail.com>
2013-11-20 13:57:40 -0600

    Update PROYECTOFINAL.java

--- PROYECTOFINAL.java
+++ PROYECTOFINAL.java
@@ -5,7 +5,64 @@ public class PROYECTOFINAL
     BufferedReader(new InputStreamReader(System.in));
   private static PrintWriter stdOut =new
     PrintWriter(System.out, true);
+  public static int principio () throws IOException
+  {
+    stdOut.println("Bienvenidos a PROYECTOFINAL 1.0");
+    stdOut.println("Creado por Mario y Tono -- MARTON SOFTWARE -- Bajo el GPLv2");
+    stdOut.println("                      .");
+    stdOut.println("                     / V\\ ");
+    stdOut.println("                   / `  /");
+    stdOut.println("                  <<   |");
+    stdOut.println("                  /    |");
+    stdOut.println("                /      |");
+    stdOut.println("              /        |");
+    stdOut.println("            /    \\  \\ /");
+    stdOut.println("           (      ) | |");
+    stdOut.println("   ________|   _/_  | |");
+    stdOut.println(" <__________\\______)\\__)");
+    int opcion=0;
+    while (opcion!=1 && opcion!=2){
+      stdOut.println("Mostrar Menu (1)");
+      stdOut.println("SALIR (2)");
+      opcion=Integer.parseInt(stdIn.readLine());
+      if(opcion!=1 && opcion!=2)
+        stdOut.println("¡Opcion invalida, intente de nuevo!");
+    }
+    return opcion;
+  }
+  public static int menu () throws IOException
+  {
+    int opcion=0;
+    while (opcion<1 || opcion>8){
+      stdOut.println("Menu Principal");
+      stdOut.println("1 ");
+      stdOut.println("2 ");
+      stdOut.println("3 ");
+      stdOut.println("4 ");
+      stdOut.println("5 ");
+      stdOut.println("6 ");
+      stdOut.println("7 ");
+      stdOut.println("8 ABOUT");
+      stdOut.println("Selecciona una opcion del menu utlizando el numero de la linea.");
+      opcion=Integer.parseInt(stdIn.readLine());
+      if(opcion<1 || opcion>8) {
+        //Se le notifica al usuario que la opcion que eliguio esta mal y se le pone un ReadLine para que pueda leerlo.
+        stdOut.println("¡Opcion invalida, intente de nuevo! (ENTER)");
+        stdIn.readLine();
+      }
+    }
+    return opcion;
+  }
   public static void main (String [] args) throws IOException
   {
+    int opcion=principio();//Se va la funcion principio donde se introduze el programa
+    if(opcion==1){
+      opcion=menu();//Se va a la funcion menu donde se le muestran todas las opciones
+//      switch(opcion) {
+//        
+//      }
+    }
+    else
+      stdOut.println("Gracias por usar nuestro programa. :) Saliendo");
   }
 }
