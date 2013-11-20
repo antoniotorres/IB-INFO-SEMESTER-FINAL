5551282e1137c2a314d3526603b4c1e656bb9e2e
Antonio <thetonio96@gmail.com>
2013-11-20 11:39:49 -0600

    Create PROYECTOFINAL.java

---
+++ PROYECTOFINAL.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class PROYECTOFINAL
+{
+  private static BufferedReader stdIn=new
+    BufferedReader(new InputStreamReader(System.in));
+  private static PrintWriter stdOut =new
+    PrintWriter(System.out, true);
+  public static void main (String [] args) throws IOException
+  {
+  }
+}
